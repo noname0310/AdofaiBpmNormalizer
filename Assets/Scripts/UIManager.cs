dbc83a608f312f4041d53b8693a43baaafeff311
noname <48761044+noname0310@users.noreply.github.com>
2021-02-25 19:00:21 +0900

    commit

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// ReSharper disable All
+
+public class UIManager : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+}
